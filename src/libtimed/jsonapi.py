0f40780a2f779877b7e589c26233226ee5fda353
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-04-09 17:15:41 +0200

    chore(jsonapi): fix complexity

--- src/libtimed/jsonapi.py
+++ src/libtimed/jsonapi.py
@@ -39,39 +39,33 @@ class ForbiddenKeyError(DeserializationError):
 
 
 def deserialize_record(record: Record, included: list[Record]):
-    if not record.get("id"):
-        raise MissingKeyError("id")
+    attributes = record.get("attributes", {})
+    relationships = record.get("relationships", {})
 
-    if not record.get("type"):
-        raise MissingKeyError("type")
+    for key in ["id", "type"]:
+        if not record.get(key):
+            raise MissingKeyError(key)
+        if attributes.get(key) or relationships.get(key):
+            raise ForbiddenKeyError(key)
 
     obj = {"id": record["id"], "type": record["type"]}
 
-    if attributes := record.get("attributes"):
-        for key in ["id", "type"]:
-            if attributes.get(key):
-                raise ForbiddenKeyError(key)
-        for key, val in attributes.items():
-            obj[key] = val
+    for key, val in attributes.items():
+        obj[key] = val
 
-    if relationships := record.get("relationships"):
-        for key in ["id", "type"]:
-            if relationships.get(key):
-                raise ForbiddenKeyError(key)
+    for key, val in relationships.items():
+        val = val["data"]
+        if included_record := next(
+            (
+                included_record
+                for included_record in included
+                if included_record["id"] == val["id"] and included_record["type"] == val["type"]
+            ),
+            None,
+        ):
+            val = deserialize_record(included_record, included)
 
-        for key, val in relationships.items():
-            val = val["data"]
-            if included_record := next(
-                (
-                    included_record
-                    for included_record in included
-                    if included_record["id"] == val["id"] and included_record["type"] == val["type"]
-                ),
-                None,
-            ):
-                val = deserialize_record(included_record, included)
-
-            obj[key] = val
+        obj[key] = val
 
     return obj
 
